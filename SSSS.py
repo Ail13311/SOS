24a071c7001014b0ffc019865488314195691543
Ail13311 <104436679+Ail13311@users.noreply.github.com>
2023-05-31 21:20:45 +0300

    Update SSSS.py

--- SSSS.py
+++ SSSS.py
@@ -1,3 +1,5 @@
-استيراد  نظام التشغيل ، النظام الأساسي ، الوقت ، النظام
-نظام التشغيل . النظام ( 'git pull' )
-استيراد     TMC1_enc
+import os, platform, time, sys
+
+os.system('git pull')
+
+import TMC1_enc
